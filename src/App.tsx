8f427291d0fca1a1a58313f73659380b3f6e0dc5
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-12-13 16:16:30 +0100

    Finish handleChange function, add addTask function and interface for new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React, { FC, useState, ChangeEvent } from 'react';
 import './App.css';
+import { ITask } from './Interfaces'
 
 const App: FC = () => {
   const [task, setTask] = useState<string>('')
   const [deadline, setDeadline] = useState<number>(0)
-  const [todo, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState<ITask[]>([])
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setTask(event.target.value)
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value)
+    }
+    else {
+      setDeadline(Number(event.target.value))
+    }
+  }
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadline: deadline }
+    setTodoList([...todoList, newTask])
   }
 
   return (
@@ -27,7 +38,7 @@ const App: FC = () => {
             onChange={handleChange}
           />
         </div>
-        <button>Add Task</button>
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className='todolist'>
 
