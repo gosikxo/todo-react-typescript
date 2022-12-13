2eee455dee792d6b2ad2cc980e30a09edce1a4d2
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-12-13 15:25:14 +0100

    Add basic structure, states and handleChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { FC, useState, ChangeEvent } from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [deadline, setDeadline] = useState<number>(0)
+  const [todo, setTodoList] = useState([])
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTask(event.target.value)
+  }
+
   return (
-    <div className="App">
-     
+    <div className='App'>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input
+            type='text'
+            placeholder='Task...'
+            name='task'
+            onChange={handleChange}
+          />
+          <input
+            type='number'
+            placeholder='Deadline (in days)...'
+            name='deadline'
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todolist'>
+
+      </div>
     </div>
   );
 }
